0795a078ee4c96cee3e7c214a6722356ebcd888c
Rohith <rohith.kontham2803@gmail.com>
2025-02-14 13:48:03 +0530

    all are being updated

---
+++ frontend/QUIZ/javascript.js
@@ -0,0 +1,467 @@
+/* Created Code by Prashanthi*/
+
+var quiz = {
+    "JS": [
+    {
+    "id": 1,
+    "question": "who was the first president of the united states?",
+    "options": [
+    {
+    "a": "George Washington",
+    "b": "Thomas Gefferson",
+    "c": "Abraham Lincoln",
+    "d": "Franklin D.Rooselevelt"
+    }
+    ],
+    "answer": "George Washington",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 2,
+    "question": "Which ancient civilization built the great Pyramid of Giza?",
+    "options": [
+    {
+    "a": "Egyptians",
+    "b": "Greeks",
+    "c": "Romanas",
+    "d": "Mesopotamians"
+    }
+    ],
+    "answer": "Egyptians",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 3,
+    "question": "Who was the first ancient roam?",
+    "options": [
+    {
+    "a": "Romulus",
+    "b": "Rumus",
+    "c": "Julius Caesar",
+    "d": "Augustus"
+
+    }
+    ],
+    "answer": "Romulus",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 4,
+    "question": "Which ancient city was the Capital of the Inca Empire?",
+    "options": [
+    {
+    "a": "Cuzco",
+    "b": "Machu Picchu",
+    "c": "Lima",
+    "d": "Santiago"
+    }
+    ],
+    "answer": "Cuzco",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 5,
+    "question": "Who was the Founder of the Mauryan Empire?",
+    "options": [
+    {
+    "a": "Chandragupta Maurya",
+    "b": "Ashoka the great",
+    "c": "Bindusara",
+    "d": "Brihadratha"
+    }
+    ],
+    "answer": "Chandragupta Maurya",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 6,
+    "question": "Which ancient Indian text is Considered the Oldest Scrpture of Hindusim?",
+    "options": [
+    {
+    "a": "Rigvedha",
+    "b": "Yajurvedha",
+    "c": "Samavedha",
+    "d": "Atharvavda"
+    }
+    ],
+    "answer": "Rigvedha",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 7,
+    "question": " Who founded the delhi sultanate in 1206 CE?",
+    "options": [
+    {
+    "a": "Qutab-ud-din Aibak",
+    "b": " Alauddin Khalji",
+    "c": "Muhammad Bin Tughlaq",
+    }
+    ],
+    "answer": "Qutab-ud-din Aibak",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 8,
+    "question": "Which Vijayanagar Emperor Built the Vittala Temple?",
+    "options": [
+    {
+    "a": "Harihara",
+    "b": "Bukkaraya",
+    "c": "Krishnadevaraya",
+    "d": " Achyutharaya"
+    }
+    ],
+    "answer": "Krishnadevaraya",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 9,
+    "question": "Who was the Founder of the Mughal Empire?",
+    "options": [
+    {
+    "a": "Babur",
+    "b": "Humayun",
+    "c": "Akbar",
+    "d": "Schah Jahan"
+    }
+    ],
+    "answer": "Babur",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 10,
+    "question": "Who let the Indian Rebellion of 1857 ?",
+    "options": [
+    {
+    "a": "Mangal Pandey",
+    "b": "Ranilakshmi bhai",
+    "c": "Tantia Tope",
+    "d": "Bhahadur Shah",
+    }
+    ],
+    "answer": "Ranilakshmi bhai",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 11,
+    "question": " Who was the First Viceroy of India?",
+    "options": [
+    {
+    "a": "Lord Canning",
+    "b": "Lord Curzon",
+    "c": "Lord Dalhoucie"
+    }
+    ],
+    "answer": "Lord canning",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 12,
+    "question": "Who lead the non-cooperation Movement in 1920-1922?",
+    "options": [
+    {
+    "a": "Mahathma Gandi",
+    "b": "Jawaharlal Nehru",
+    "c": "Subash Chandrabose",
+    "d": "Lala Lajpat Rai"
+    }
+    ],
+    "answer": "Mahathma Gandi",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 13,
+    "question": "Who was India's First Prime Minister?",
+    "options": [
+    {
+    "a": "Jawaharlal Nehru",
+    "b": "Subhas Chandrabose",
+    "c": "Mahatma Gandi"
+    }
+    ],
+    "answer": "Jawaharlal Nehru",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 14,
+    "question": "Who was the Architect of indians Constitution?",
+    "options": [
+    {
+    "a": "Jawaharlal Nehru",
+    "b": "B R.Ambedkar",
+    "c": "Mahatma Gandi"
+    }
+    ],
+    "answer": "B R.Ambedkar",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 15,
+    "question": "Who was the Founder of the Guptha Empire?",
+    "options": [
+    {
+    "a": "Siri Guptha",
+    "b": "Ghatotkacha",
+    "c": "Chandra Guptha-1",
+    "d": "Samudra Guptha"
+    }
+    ],
+    "answer": "Chandra Guptha-1",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 16,
+    "question": "Which ancient Indian Text Describies the Life of Buddha ?",
+    "options": [
+    {
+    "a": "Mahabaratha",
+    "b": "Ramayana",
+    "c": "Puranas",
+    "d": "Pali Canon"
+    }
+    ],
+    "answer": "Pali Canon",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 17,
+    "question": " Who was the Mughal Emperor who bulit the Tajmahal?",
+    "options": [
+    {
+    "a": "Akbar",
+    "b": "Jahangir",
+    "c": "Shah Jahan",
+    "d": "Aurangzeb"
+    }
+    ],
+    "answer":"Shah Jahan",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 18,
+    "question": "Who was the First Indian to become a Member of the British Parliament?",
+    "options": [
+    {
+    "a": "Dadadhai Naoroji",
+    "b": "Lala lajpat Rai"
+    }
+    ],
+    "answer": "Dadadhai Naoroji",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 19,
+    "question": "Who founded the Indian National Congress in 1885?",
+    "options": [
+    {
+    "a": "Lala lajpat Rai",
+    "b": "Lallan Octavian hume"
+    }
+    ],
+    "answer": "Lallan Octavian hume",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 20,
+    "question": " Who was the Indian's Second Prime Minister?",
+    "options": [
+    {
+    "a": "Jawaharlal Nehru",
+    "b": "Gulzarilal Nanda",
+    "c": "Lal Bahadur Shastri"
+    }
+    ],
+    "answer": "Lal Bahadur Shastri",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 21,
+    "question": " Who was the first President of India",
+    "options": [
+    {
+    "a": "Rajendra Prasad",
+    "b": "Saravapelli Radhakrishanan",
+    "c": "Zakir Husain",
+    "d": "V.v.Giri"
+    }
+    ],
+    "answer": "Rajendra Prasad",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 22,
+    "question": " Who was the Founder of the Pala Empire?",
+    "options": [
+    {
+    "a": "Gopala",
+    "b": "Daramapala",
+    "c": "Devapala"
+    }
+    ],
+    "answer": "Gopala",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 23,
+    "question": "Who founded the Sultanate of Malwa in 1401 CE?",
+    "options": [
+    {
+    "a": "Hoshang Shah",
+    "b": "Dilawaar khan",
+    "c": "Mahumhat",
+    "d": "Baz bahadur"
+    }
+    ],
+    "answer": "Dilawaar khan",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 24,
+    "question": "Who was the founder of the Maratha Empire? ",
+    "options": [
+    {
+    "a": "Shivaji",
+    "b": "Sambhaji",
+    "c": "Rajaram"
+    }
+    ],
+    "answer": "Shivaji",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 25,
+    "question": "Who was the First Cheif Justice of India ?",
+    "options": [
+    {
+    "a": "A.N. Roi",
+    "b": "H.J.Kania"
+    }
+    ],
+    "answer": "H.J.kania",
+    "score": 0,
+    "status": ""
+    }
+    ]
+    }
+    var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function (cque) {
+    this.currentque = cque;
+    if (this.currentque < totalque) {
+    $("#tque").html(totalque);
+    $("#previous").attr("disabled", false);
+    $("#next").attr("disabled", false);
+    $("#qid").html(quiz.JS[this.currentque].id + '.');
+    $("#question").html(quiz.JS[this.currentque].question);
+    $("#question-options").html("");
+    for (var key in quiz.JS[this.currentque].options[0]) {
+    if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+    $("#question-options").append(
+    "<div class='form-check option-block'>" +
+    "<label class='form-check-label'>" +
+    "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+    quiz.JS[this.currentque].options[0][key] +
+    "</span></label>"
+    );
+    }
+    }
+    }
+    if (this.currentque <= 0) {
+    $("#previous").attr("disabled", true);
+    }
+    if (this.currentque >= totalque) {
+    $('#next').attr('disabled', true);
+    for (var i = 0; i < totalque; i++) {
+    this.score = this.score + quiz.JS[i].score;
+    }
+    return this.showResult(this.score);
+    }
+    }
+    this.showResult = function (scr) {
+    $("#result").addClass('result');
+    $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+    for (var j = 0; j < totalque; j++) {
+    var res;
+    if (quiz.JS[j].score == 0) {
+    res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+    } else {
+    res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+    }
+    $("#result").append(
+    '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+    '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+    '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+    '</div>'
+    );
+    }
+    }
+    this.checkAnswer = function (option) {
+    var answer = quiz.JS[this.currentque].answer;
+    option = option.replace(/</g, "&lt;") //for <
+    option = option.replace(/>/g, "&gt;") //for >
+    option = option.replace(/"/g, "&quot;")
+    if (option == quiz.JS[this.currentque].answer) {
+    if (quiz.JS[this.currentque].score == "") {
+    quiz.JS[this.currentque].score = 1;
+    quiz.JS[this.currentque].status = "correct";
+    }
+    } else {
+    quiz.JS[this.currentque].status = "wrong";
+    }
+    }
+    this.changeQuestion = function (cque) {
+    this.currentque = this.currentque + cque;
+    this.displayQuiz(this.currentque);
+    }
+    }
+    var jsq = new quizApp();
+    var selectedopt;
+    $(document).ready(function () {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+    //var radio = $(this).find('input:radio');
+    $(this).prop("checked", true);
+    selectedopt = $(this).val();
+    });
+    });
+    $('#next').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+    });
+    $('#previous').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+    });
